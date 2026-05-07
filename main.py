d83fddc7a3e5e7e7c72cce6d681508a91d0367ab
guanjie <gjsun255@163.com>
2026-05-07 11:54:24 +0800

    调用本地 ollama 大模型

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from learn-ollama!")
+from ollama import chat
 
+def main():
+    stream = chat(
+        model='qwen3:8b',
+        messages=[{'role': 'user', 'content': '如何将 AI 与 RPA 融合？'}],
+        stream=True,
+    )
+
+    for chunk in stream:
+        print(chunk['message']['content'], end='', flush=True)
 
 if __name__ == "__main__":
     main()
