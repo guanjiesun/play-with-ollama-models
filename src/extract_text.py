5daf1528696ff4bd5b5d28da6e4f066e284d3ca1
guanjie <gjsun255@163.com>
2026-05-14 16:46:04 +0800

    使用qwen3.5:2b模型

--- src/extract_text.py
+++ src/extract_text.py
@@ -5,9 +5,9 @@ def main():
     """
     extract text from an image file
     """
-    image_path = Path('../static/image.png')
+    image_path = Path('../static/image1.png')
     stream = chat(
-        model='qwen3.5:9b',
+        model='qwen3.5:2b',
         messages=[{'role': 'user', 'content': '提取图片中的文字', 'images': [image_path]}],
         stream=True,
     )
@@ -16,4 +16,11 @@ def main():
         print(chunk['message']['content'], end='', flush=True)
 
 if __name__ == "__main__":
-    main()
+    import time
+    start = time.time()
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    end = time.time()
+    print(f'\n\n程序运行 {round(end-start, 2)}s')
