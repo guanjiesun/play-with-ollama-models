d1b58b8976b544f2dc7d0b633afd3f4dc1a78611
guanjie <gjsun255@163.com>
2026-05-26 16:50:57 +0800

    specify input folder path

--- src/extract_text.py
+++ src/extract_text.py
@@ -1,11 +1,13 @@
 from ollama import chat
 from pathlib import Path
 
+INPUT_DIR = Path(__file__).parent.parent / 'inputs'
+
 def main():
     """
     extract text from an image file
     """
-    image_path = Path('../static/image1.png')
+    image_path = INPUT_DIR / 'image.png'
     stream = chat(
         model='qwen3.5:2b',
         messages=[{'role': 'user', 'content': '提取图片中的文字', 'images': [image_path]}],
@@ -16,11 +18,7 @@ def main():
         print(chunk['message']['content'], end='', flush=True)
 
 if __name__ == "__main__":
-    import time
-    start = time.time()
     try:
         main()
     except Exception as e:
         print(e)
-    end = time.time()
-    print(f'\n\n程序运行 {round(end-start, 2)}s')
